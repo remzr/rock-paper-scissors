c57bada5897305ed4cb1073a4b412a585fb99653
remzr <110097561+remzr@users.noreply.github.com>
2025-08-30 14:40:43 +0200

    Create exectuion and basic game loop

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,23 +1,41 @@
 let humanScore = 0;
 let computerScore = 0;
 
-let getComputerChoice = function () {
+function getComputerChoice () {
     let getRandomInt = Math.floor(Math.random() * 3)
 
     if (getRandomInt == 0) {
-        return "Rock";
+        return "rock";
     } 
     else if (getRandomInt == 1) {
-        return "Paper";
+        return "paper";
     }   
     else {
-        return "Scissors";
+        return "scissors";
     }
 }
 
-let humanChoice = function getHumanChoice () {
-    let humanInput = prompt("Rock, Paper, Scissors?");
-    console.log(humanInput)
+function getHumanChoice () {
+    let humanInput = (prompt("Rock, Paper, Scissors?")).toLowerCase();
     return humanInput;
 }
 
+//Game Loop
+function playRound (humanChoice, computerChoice) {
+    
+    //In case of a draw
+    if (humanChoice == computerChoice) {
+        console.log(`${humanChoice} against ${computerChoice}. Its a draw!`);
+
+    } else {
+        console.log("The game continues...");
+    }
+}
+
+//Execute
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+console.log(`${humanSelection} VS ${computerSelection}`)
+
+playRound(humanSelection, computerSelection);
