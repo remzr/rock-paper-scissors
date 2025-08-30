d6ac8fa17275b6be73baf41ff6399d33d6784858
remzr <110097561+remzr@users.noreply.github.com>
2025-08-30 15:02:06 +0200

    Finish and debug game loop. Add point counter

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -26,11 +26,38 @@ function playRound (humanChoice, computerChoice) {
     //In case of a draw
     if (humanChoice == computerChoice) {
         console.log(`${humanChoice} against ${computerChoice}. Its a draw!`);
-
-    } else {
-        console.log("The game continues...");
+    }
+    //Player wins
+    else if (humanChoice == "rock" && computerChoice == "scissors") {
+            humanScore++;
+            console.log(`${humanChoice} beats ${computerChoice}. Player wins!`);
+    }
+    else if (humanChoice == "scissors" && computerChoice == "paper") {
+            humanScore++;
+            console.log(`${humanChoice} beats ${computerChoice}. Player wins!`);
+    }
+    else if (humanChoice == "paper" && computerChoice == "rock") {
+            humanScore++;
+            console.log(`${humanChoice} beats ${computerChoice}. Player wins!`);
+    }
+    //Computer wins
+    else if (humanChoice == "rock" && computerChoice == "paper") {
+            computerScore++;
+            console.log(`${computerChoice} beats ${humanChoice}. Computer wins!`);
+    }
+    else if (humanChoice == "scissors" && computerChoice == "rock") {
+            computerScore++;
+            console.log(`${computerChoice} beats ${humanChoice}. Computer wins!`);
+    }
+    else if (humanChoice == "paper" && computerChoice == "scissors") {
+            computerScore++;
+            console.log(`${computerChoice} beats ${humanChoice}. Computer wins!`);
+    }
+    else {
+            console.log(`There might have been a Typo.`);
     }
 }
+
 
 //Execute
 const humanSelection = getHumanChoice();
