9d23a91fc93bda6b32e2571157a971cb0fd4aa1d
remzr <110097561+remzr@users.noreply.github.com>
2025-08-31 20:52:03 +0200

    Fix return values and call order of functions. Working git push git@github.com:remzr/rock-paper-scissors.git q bash

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,9 +1,14 @@
-let humanScore = 0;
-let computerScore = 0;
-let humanSelection = 0;
-let comuterSelection = 0;
+let humanScore;
+let computerScore;
 
-/*function getComputerChoice () {
+//Functions to set human and computer choice of rock, paper or scissors
+
+function getHumanChoice () {
+    let humanInput = (prompt("Rock, Paper, Scissors?")).toLowerCase();
+    return humanInput;
+}
+
+function getComputerChoice() {
     let getRandomInt = Math.floor(Math.random() * 3)
 
     if (getRandomInt == 0) {
@@ -15,72 +20,67 @@ let comuterSelection = 0;
     else {
         return "scissors";
     }
-} */
+}
 
-function getHumanChoice () {
-    let humanInput = (prompt("Rock, Paper, Scissors?")).toLowerCase();
-    return humanInput;
+function roundResult(humanChoice, computerChoice) {
+    //console.log(`Round started. Human-Score: ${computerScore} ComputerScore: ${humanScore}`);
+
+    //In case of a draw
+    if (humanChoice == computerChoice) {
+        console.log(`${humanChoice} against ${computerChoice}. Its a draw!`);
+    }
+
+    //Player wins
+    else if (humanChoice == "rock" && computerChoice == "scissors") {
+        humanScore++;
+        console.log(`${humanChoice} beats ${computerChoice}. Player wins!`);
+    }
+    else if (humanChoice == "scissors" && computerChoice == "paper") {
+        humanScore++;
+        console.log(`${humanChoice} beats ${computerChoice}. Player wins!`);
+    }
+    else if (humanChoice == "paper" && computerChoice == "rock") {
+        humanScore++;
+        console.log(`${humanChoice} beats ${computerChoice}. Player wins!`);
+    }
+
+    //Computer wins
+    else if (humanChoice == "rock" && computerChoice == "paper") {
+        computerScore++;
+        console.log (`${computerChoice} beats ${humanChoice}. Computer wins!`);
+    }
+    else if (humanChoice == "scissors" && computerChoice == "rock") {
+        computerScore++;
+        console.log (`${computerChoice} beats ${humanChoice}. Computer wins!`);
+    }
+    else if (humanChoice == "paper" && computerChoice == "scissors") {
+        computerScore++;
+        console.log (`${computerChoice} beats ${humanChoice}. Computer wins!`);
+    }
+    else {
+        console.log (`There might have been a Typo.`);
+    }
 }
 
 //Game Loop
-
 function playGame() {
+    
+    humanScore = 0;
+    computerScore = 0;
+    
     for (let i = 0; i < 5; i++) {
-            
-        let computerSelection = function getComputerChoice () {
-            let getRandomInt = Math.floor(Math.random() * 3)
-
-            if (getRandomInt == 0) {
-                return "rock";
-            } 
-            else if (getRandomInt == 1) {
-                return "paper";
-            }   
-            else {
-                return "scissors";
-            }
-        }
-
+        
+        let computerSelection = getComputerChoice();
         let humanSelection = getHumanChoice();
         
-        console.log(`${humanSelection} VS ${computerSelection}`)
-       
-        return function playRound(humanChoice, computerChoice) {
-
-            //In case of a draw
-            if (humanChoice == computerChoice) {
-                console.log(`${humanChoice} against ${computerChoice}. Its a draw!`);
-            }
-            //Player wins
-            else if (humanChoice == "rock" && computerChoice == "scissors") {
-                humanScore++;
-                return (`${humanChoice} beats ${computerChoice}. Player wins!`);
-            }
-            else if (humanChoice == "scissors" && computerChoice == "paper") {
-                humanScore++;
-                return (`${humanChoice} beats ${computerChoice}. Player wins!`);
-            }
-            else if (humanChoice == "paper" && computerChoice == "rock") {
-                humanScore++;
-                return (`${humanChoice} beats ${computerChoice}. Player wins!`);
-            }
-            //Computer wins
-            else if (humanChoice == "rock" && computerChoice == "paper") {
-                computerScore++;
-                return (`${computerChoice} beats ${humanChoice}. Computer wins!`);
-            }
-            else if (humanChoice == "scissors" && computerChoice == "rock") {
-                computerScore++;
-                return (`${computerChoice} beats ${humanChoice}. Computer wins!`);
-            }
-            else if (humanChoice == "paper" && computerChoice == "scissors") {
-                computerScore++;
-                return (`${computerChoice} beats ${humanChoice}. Computer wins!`);
-            }
-            else {
-                return (`There might have been a Typo.`);
-            }
+        console.log(`${humanSelection} VS ${computerSelection}`)       
+        roundResult(computerSelection, humanSelection);
         }
+
+    if (computerScore > humanScore) {
+        console.log(`Computer has ${computerScore}, Player has ${humanScore} Points. Computer wins!`);
+    } else {
+        console.log(`Player has ${humanScore} Points, Computer has ${computerScore}. Player wins!`);
     }
 }
 
